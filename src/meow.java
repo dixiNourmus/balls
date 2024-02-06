21f87a1a97d681691f704c9a9aeb2e4abafbee5c
dixiNourmus <joshua.berger@talawanda.org>
2024-02-06 13:46:15 -0500

    test commit

--- src/meow.java
+++ src/meow.java
@@ -2,7 +2,11 @@ public class meow {
 
     public static  void main(String[] args) {
 
-        System.out.println("hello world");
+        int myTotal = 0;
+
+        myTotal = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+        System.out.println("your total is " + myTotal);
+
 
     }
 
