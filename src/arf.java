ef6c1eb71ea7c9ed64f10e4ab3853ec6b6251b8f
dixiNourmus <joshua.berger@talawanda.org>
2024-02-06 13:53:06 -0500

    test commit

---
+++ src/arf.java
@@ -0,0 +1,10 @@
+public class arf {
+
+    public static  void main(String[] args) {
+
+        int total = 0;
+        total = 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+        System.out.println("your total is " + total);
+
+    }
+}
